afc87fd1b0e996074482210f96506bbda443c002
Matthew Jording <mjording@opengotham.com>
2011-03-09 00:38:05 -0500

    omniauth callback hook

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,16 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-   def self.new_with_session(params, session)
+  def self.find_for_twitter_oauth(access_token, signed_in_resource=nil)
+    data = access_token['extra']['user_hash']
+    if user = User.find_by_email("#{data['nickname']}@twitter.com")
+      user
+    else # Create an user with a stub password. 
+      User.create!(:email => "#{data['nickname']}@twitter.com", :password => Devise.friendly_token[0,20]) 
+    end
+  end 
+
+  def self.new_with_session(params, session)
     super.tap do |user|
       if data = session[:omniauth]
         logger.error("in new_with_session of user #{data.to_yaml}")
