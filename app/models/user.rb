f06f0a2d8f61454c71a274935714885aac26881c
Daniel Walmsley <goldsounds@gmail.com>
2011-03-09 02:42:48 -0500

    committing before waking up matt

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :twiter_id, :avatar_url
+  
   def self.find_for_twitter_oauth(access_token, signed_in_resource=nil)
     data = access_token['extra']['user_hash']
     puts "** data: #{data.inspect}"
