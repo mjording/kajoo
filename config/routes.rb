f004177f8208007912a86e1c78eb494d3f7b4deb
Matthew Jording <mjording@opengotham.com>
2011-03-10 14:11:23 -0600

    corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,17 +10,17 @@ FixIt::Application.routes.draw do
   match 'issues/:id/vote' => 'issues#vote', :as => 'vote_for_issue'
 
   resources :reports
-
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+ 
   get "pages/index"
   get "pages/dashboard"
   get "pages/home"
   get "pages/contact"
   get "pages/about"
-
-  resources :users
   get "users/dashboard"
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  # You can have the root of your site routed with "root"
+  
+  resources :users
+   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => "pages#index"
 
