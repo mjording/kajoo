16bd540e0b28893681072ba029defcdf4f4c09ca
Matthew Jording <mjording@opengotham.com>
2011-03-10 02:55:21 -0600

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110309210727) do
+ActiveRecord::Schema.define(:version => 20110310054739) do
 
   create_table "comments", :force => true do |t|
     t.integer  "issue_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20110309210727) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "address"
+    t.integer  "report_id"
     t.boolean  "resolved"
   end
 
