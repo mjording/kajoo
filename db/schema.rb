46c36cc80f16dd3f4b892c8b5883777387243fbf
Matthew Jording <mjording@opengotham.com>
2011-03-10 00:26:24 -0600

    migrate & bundle

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20110309210727) do
+
   create_table "issues", :force => true do |t|
     t.string   "title"
     t.text     "description"
@@ -53,6 +54,7 @@ ActiveRecord::Schema.define(:version => 20110309210727) do
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
     t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
