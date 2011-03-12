ae689d5620893811885b0d5c9cce6a960f743a96
Matthew Jording <mjording@opengotham.com>
2011-03-12 14:34:30 -0600

    fix for issue vote

--- app/models/report_observer.rb
+++ app/models/report_observer.rb
@@ -11,6 +11,7 @@ class ReportObserver < ActiveRecord::Observer
     puts "Doing after create for report #{report.title}"
     #begin 
     issue = Issue.create(:title => report.title, :description => report.description, :lat => report.lat, :lon => report.lon, :radius => report.radius)
+    issue.add_vote_for_user(report.user)
     #  report.issue = issue
     #  issue.add_vote_for_user(report.user)
     #  issue.save!
