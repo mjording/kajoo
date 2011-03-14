2e38b21a6ba01e6ba21863c03b0ba047cbd085fe
Daniel Walmsley <goldsounds@gmail.com>
2011-03-14 19:05:18 -0400

    achievement refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,10 +34,6 @@ module ApplicationHelper
     (SITE['units'] == 'mi') ? 'miles' : 'kms'
   end
   
-  def available_achievements
-    return Achievement::ACHIEVEMENTS
-  end
-  
   def summary_date(date)
     #March 8, 2011
     date.strftime('%B %d, %Y')
