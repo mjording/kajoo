981263d8e79b35ae7a38fddd54e5ad9651a04e52
Matthew Jording <mjording@opengotham.com>
2011-03-15 17:29:46 -0500

    find user by twitter or userid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   #end
   
   def show
-    @user = User.find_by_twitter_username(params[:userid])
+    
+    @user = User.find_by_twitter_username(params[:userid]) || User.find(params[:userid])
     @issues = @user.reports.map{|r|r.issue}
     @reports = @user.reports.paginate :page => params[:page], :order => 'created_at DESC', :per_page => 10
   end
