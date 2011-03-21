08b46ff0abb3334d8b1105d326b6e9e59f0eec55
Daniel Walmsley <goldsounds@gmail.com>
2011-03-21 09:05:51 -0400

    set and cache user location on page load, periodically refresh, adds responsiveness and means we can customise the whole interface for their location, not just the report form

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Kajoo::Application.routes.draw do
   resources :users
   
   match 'user/location' => 'users#location'
+  match 'user/address' => 'users#address'
   
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
