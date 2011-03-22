c0a7a45d2eca79a565c0b150369b107a36883f3c
Matthew Jording <mjording@opengotham.com>
2011-03-22 16:11:50 -0400

    minor fixes post merge master

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,7 +18,7 @@ class PagesController < ApplicationController
      @issues = Issue.near(site_location, site_radius).page(params[:page]).per(5)
  
      @submitted_reports = Report.all.count
-    @resolved_reports = Issue.where(:resolved => true).join(:reports).count('reports')
+    #@resolved_reports = Issue.where(:resolved => true).join(:reports).count('reports')
     @user_count = User.all.count
     
     chart = GoogleChart.bar_vertical_stacked_300x100(10,20,15,50,5,15,22,25,19,60)
