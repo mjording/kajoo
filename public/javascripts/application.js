9cf2877d75526cbaacccada55111f4a2e395abc1
Moudy <moudy.elkammash@gmail.com>
2011-03-29 14:11:04 -0400

    organized main app js file into one global Kajoo object

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,77 +1,205 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+Kajoo = (function (jQuery) {
 
-//cached user location - XXX TODO retrieve from cookie, encode securely
-var user_lat;
-var user_lon;
-var user_address;
-var user_location_updated = 0;
-var user_location_delay = 2000000;//1000 * 60 * 60; // 1 hour, in milliseconds
-var exdays = 1; //location cookie expiry
+  var Location = (function () {
 
-$(document).ready(function(){
+    //cached user location - XXX TODO retrieve from cookie, encode securely
+    var user_lat,
+      user_lon,
+      user_address,
+      user_location_updated = 0,
+      user_location_delay = 2000000,//1000 * 60 * 60; // 1 hour, in milliseconds
+      exdays = 1, //location cookie expiry
+      $location,
 
-  //clearinginput on report form elements  
-  $('.report input:text').clearingInput();
-  $('.report textarea').clearingInput();
-  
-  //fade out alerts  
-  $('#notices').delay(2000).slideUp(1000);
-  
-  $('#location').live('click', function(){updateUserLocation(true)});
-  
-  user_lat = $.cookie('lat') / 1;
-  user_lon = $.cookie('lon') / 1;
-  user_address = $.cookie('address');
-  user_location_updated = $.cookie('location_updated');
-  
-  updateUserLocation();
-});
+    // these getter/setter mehtods seem redundant
+    // couldn't we access and assign the values directly or am I missing something?
+    getUserLat = function () { return user_lat / 1; },
+    getUserLon = function () { return user_lon / 1; },
+    getUserAddress = function () { return user_address; },
+    setUserLocationUpdated = function (time) { user_location_updated = time; },
+    setUserLat = function (lat) { user_lat = lat / 1; },
+    setUserLon = function (lon) { user_lon = lon / 1; },
+    setUserAddress = function (address) { user_address = address; },
+
+    userLocationError = function () {
+      $location.html("Err");
+    },
+
+    locationLabel = function (msg) {
+      return '<img src="/images/globe.png" alt="'+msg+'" width="20" height="20"/>';
+    },
+
+    showUserAddress = function (address) {
+      $location.html(locationLabel(address));
+    },
+
+    showUserLocation = function (lat, lon) {
+      $location.html(locationLabel(lat+','+lon));
+    },
+
+    updateUserLocation = function (force) {
+      var lastupdated = user_location_updated,
+        threshold = new Date().getTime() - user_location_delay,
+
+      shouldUpdate = function() {
+        return ( (user_location_updated < new Date().getTime() - user_location_delay) || force );
+      };
+
+      //alert('compare '+lastupdated+' to '+threshold+' = '+(threshold-lastupdated));
+      if(shouldUpdate()) {
+        updateLocationData();
+      } else {
+        showUserLocation(getUserLat(), getUserLon());
+        showUserAddress(getUserAddress());
+      }
+    },
+
+    updateLocationData = function () {
+      if(geo_position_js.init()) {
+        $location.html("<img src=\"/images/spinner.gif\"/>");
+        geo_position_js.getCurrentPosition(setUserLocation,userLocationError);//{enableHighAccuracy:true}
+      } else {
+        $location.html("Automatic Location not available");
+      }
+      //we need to update this anyway so that it doesn't try to get location on _every_ page load.
+      setUserLocationUpdated(new Date().getTime());
+    },
+
+    //callback from geo.js
+    setUserLocation = function (p) {
+
+      var lat = p.coords.latitude,
+      lon = p.coords.longitude,
+      pos=new google.maps.LatLng(lat,lon);
+
+      showUserLocation(lat, lon);
+      geocodeAddress(pos);
+
+      /*
+         setCookie('long', user_lon);
+         setCookie('lat', user_lat);
+         setCookie('location_updated', new Date().getTime());
+         */
+
+      var loc = {lat: lat / 1, lon: lon / 1}; // lat, lon
+      //ping server
+      $.ajax({
+        url: '/user/location',
+        async: true,
+        data: loc,
+        dataType: 'script',
+        complete: function( res, status ) {
+          //alert("set location");
+        }
+      });
+      setUserLocationUpdated(new Date().getTime());
+    },
+
+    geocodeAddress = function(pos) {
+      //Geocode to address. Oh yes.
+      var geocoder = new google.maps.Geocoder(),
+
+      geocodeSuccess = function(results) {
+        var address = results[0].formatted_address;
+        showUserAddress(address);
+
+        //      setCookie('address', address);
+        //      setCookie('address_updated', new Date().getTime());
+        var addr_param = {address: address};
+        //ping server
+        $.ajax({
+          url: '/user/address',
+          async: true,
+          data: addr_param,
+          dataType: 'script',
+          complete: function( res, status ) {
+            //setUserAddress(address);
+          }
+        });
+      },
+
+      geocodeDone = function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          geocodeSuccess(results);
+        }
+      };
+
+      geocoder.geocode({'latLng': pos}, geocodeDone);
+
+    },
 
 
-function updateUserLocation(force) {
-  var lastupdated = getUserLocationUpdated();
-  var threshold = new Date().getTime() - user_location_delay;
-  
-  //alert('compare '+lastupdated+' to '+threshold+' = '+(threshold-lastupdated));
-  
-  
-  if((getUserLocationUpdated() < new Date().getTime() - user_location_delay) || force) {
-    
-    if(geo_position_js.init())
-  	{
-  		$('#location').html("<img src=\"/images/spinner.gif\"/>");
-  		geo_position_js.getCurrentPosition(setUserLocation,userLocationError);//{enableHighAccuracy:true}
-  	}
-  	else
-  	{
-  		$('#location').html("Automatic Location not available");
-  	}
-  	
-  	//we need to update this anyway so that it doesn't try to get location on _every_ page load.
-  	
-  	setUserLocationUpdated(new Date().getTime());
-  } else {
-    showUserLocation(getUserLat(), getUserLon());
-    showUserAddress(getUserAddress());
-  }
-}
+    init = function () {
+      $location = $('#location');
+      user_lat = $.cookie('lat') / 1;
+      user_lon = $.cookie('lon') / 1;
+      user_address = $.cookie('address');
+      user_location_updated = $.cookie('location_updated');
 
-function showNavBar() {
- var win_y = $(window).height();
- var scroll_y = $(window).scrollTop();
- $("footer").css({ top: ((win_y - 36) + scroll_y) + "px" });
- $("header").css({ top: scroll_y + "px" });
-}
+      $location.live('click', function(){updateUserLocation(true)});
+      updateUserLocation();
+    };
 
-var showTimer = false;
+    return { init:init,
+             getUserLat:getUserLat,
+             getUserLon:getUserLon,
+             getUserAddress:getUserAddress,
+             setUserAddress:setUserAddress }
 
-function maybeShowNavBar(evt) {
- if ( showTimer ) {
-   clearTimeout( showTimer );
- }
- showTimer = setTimeout( showNavBar, 175 );
-}
+  }()); // Location END
+
+  NavBar = (function () {
+    var showTimer = false,
+
+    showNavBar = function () {
+     var win_y = $(window).height(),
+       scroll_y = $(window).scrollTop();
+     $("footer").css({ top: ((win_y - 36) + scroll_y) + "px" });
+     $("header").css({ top: scroll_y + "px" });
+    },
+
+    maybeShowNavBar = function (evt) {
+      showTimer ? clearTimeout(showTimer) : '';
+      showTimer = setTimeout( showNavBar, 175 );
+    },
+
+    init = function() {
+      // doesn't do anything yet
+    };
+
+    return { init:init }
+  }()); // NavBar END
+
+
+  var init = function () {
+    Location.init();
+    //clearinginput on report form elements
+    $('.report input:text').clearingInput();
+    $('.report textarea').clearingInput();
+
+    //fade out alerts
+    $('#notices').delay(2000).slideUp(1000);
+
+  };
+
+  // document ready shorthand, fires init method above
+  $(init);
+
+  // makes Location object accesable from ouside global Kajoo object
+  return { Location:Location }
+
+}($));
+
+
+
+
+
+
+
+
+
+
+
 
 /**
  * extend jquery with "$('elem').scrollToMe();
@@ -84,7 +212,7 @@ function maybeShowNavBar(evt) {
 			height: this.offset().top
 		}, 'fast');
 	}
-  
+
   $.cookie = function(key, value, options){
   // key and at least value given, set cookie...
   if (arguments.length > 1 && String(value) !== "[object Object]") {
@@ -112,7 +240,7 @@ function maybeShowNavBar(evt) {
     var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
     return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
   };
-  
+
   $.fn.animateHighlight = function(highlightColor, duration) {
     var highlightBg = highlightColor || "#FFFF9C";
     var animateMs = duration || 1500;
@@ -120,107 +248,4 @@ function maybeShowNavBar(evt) {
     this.stop().css("background-color", highlightBg).animate({backgroundColor: originalBg}, animateMs);
   };
 })(jQuery);
-
-//callback from geo.js
-function setUserLocation(p)
-{
-
-  var lat = p.coords.latitude;
-  var lon = p.coords.longitude;
-  
-  var pos=new google.maps.LatLng(lat,lon);
-
-  showUserLocation(lat, lon);
-
-  //Geocode to address. Oh yes.
-  geocoder = new google.maps.Geocoder();
-  geocoder.geocode({'latLng': pos}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      var address = results[0].formatted_address;
-      showUserAddress(address);
-      
-//      setCookie('address', address);
-//      setCookie('address_updated', new Date().getTime());
-      var addr_param = {address: address};
-      //ping server
-      $.ajax({
-        url: '/user/address',
-        async: true,
-        data: addr_param,
-        dataType: 'script',
-        complete: function( res, status ) {
-          //setUserAddress(address);
-        }
-      });
-    }
-  });	
-  
-/*
-  setCookie('long', user_lon);
-  setCookie('lat', user_lat);
-  setCookie('location_updated', new Date().getTime());
-*/
-  
-  var loc = {lat: lat / 1, lon: lon / 1}; // lat, lon
-  //ping server
-  $.ajax({
-    url: '/user/location',
-    async: true,
-    data: loc,
-    dataType: 'script',
-    complete: function( res, status ) {
-      //alert("set location");
-    }
-  });
-  setUserLocationUpdated(new Date().getTime());
-}
-
-function showUserAddress(address) {
-  $('#location').html(locationLabel(address));
-}
-
-function showUserLocation(lat, lon) {
-  $('#location').html(locationLabel(lat+','+lon));
-}
-
-function locationLabel(msg) {
-  return '<img src="/images/globe.png" alt="'+msg+'" width="20" height="20"/>';
-}
-
-function userLocationError() {
-  $('#location').html("Err");
-}
-
-function getUserLat() {
-  return user_lat / 1;
-}
-
-function getUserLon() {
-  return user_lon / 1;
-}
-
-function getUserAddress() {
-  return user_address;
-}
-
-//ms since 1/1/1970
-function getUserLocationUpdated() {
-  return user_location_updated;
-}
-
-function setUserLocationUpdated(time) {
-  user_location_updated = time;
-}
-
-function setUserLat(lat) {
-  user_lat = lat / 1;
-}
-
-function setUserLon(lon) {
-  user_lon = lon / 1;
-}
-
-function setUserAddress(address) {
-  user_address = address;
-}
 
